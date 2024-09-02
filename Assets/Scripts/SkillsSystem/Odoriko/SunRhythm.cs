1f7625bc98b383dd7462a8a82e9ea6e209d3e9e2
Clark Ward <67551381+ClarkWard711@users.noreply.github.com>
2024-09-02 19:21:40 +0800

    0.1.25.0
     增加了日之律动脚本

---
+++ Assets/Scripts/SkillsSystem/Odoriko/SunRhythm.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunRhythm : OdorikoSkill
+{
+    public SunRhythm()
+    {
+        SpCost = 8;
+        odoSkillKind = OdoSkillKind.Sun;
+    }
+}
