8b2fe1fe3184e79fdd51ef98f6b57f65b866eb71
Clark Ward <67551381+ClarkWard711@users.noreply.github.com>
2024-11-13 00:07:29 +0800

    0.2.2.17

--- Assets/Scripts/SkillsSystem/Bloodist/Solo.cs
+++ Assets/Scripts/SkillsSystem/Bloodist/Solo.cs
@@ -4,4 +4,9 @@ using UnityEngine;
 
 public class Solo : BloodistSkill
 {
+    public Solo()
+    {
+        SpCost = 15;
+        bloodistSkillKind = BloodistSkillKind.other;
+    }
 }
--- Assets/Scripts/TagSystem/Tags/BloodistTag/SoloTag.cs
+++ Assets/Scripts/TagSystem/Tags/BloodistTag/SoloTag.cs
@@ -6,6 +6,13 @@ public class SoloTag : Tag
 {
     public SoloTag()
     {
-
+        TagName = "SoloTag";
+        TagKind = Kind.turnLessen;
+        TurnAdd = 2;
+        TurnLast = 2;
+        Effect = effect.good;
+        BuffTarget = target.self;
+        Impact = impactOnMultiplier.AllTake;
+        Multiplier = 0.8f;
     }
 }
