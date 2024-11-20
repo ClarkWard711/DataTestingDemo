38de17c41c59e7abfc78e28be620834eff48a16e
Clark Ward <67551381+ClarkWard711@users.noreply.github.com>
2024-11-21 00:06:18 +0800

    0.3.0
    随机地图生成完善

---
+++ Assets/DungeonMap/Examples/Grid2D/Example1/Scripts/GenerateTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Edgar.Unity;
+using UnityEngine;
+
+public class GenerateTest : MonoBehaviour
+{
+	public GameObject generator;
+	public int seed;
+
+	public void Awake()
+	{
+		generator.GetComponent<DungeonGeneratorBaseGrid2D>().RandomGeneratorSeed = seed;
+		//seed = generator.GetComponent<DungeonGeneratorBaseGrid2D>().RandomGeneratorSeed;
+		//Debug.Log(seed);
+		//Debug.Log(generator.GetComponent<DungeonGeneratorBaseGrid2D>().RandomGeneratorSeed);
+	}
+}
