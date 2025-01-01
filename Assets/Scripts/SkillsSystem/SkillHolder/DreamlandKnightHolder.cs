4d6acd7117fa1dfe083f8782aa3e4fb69ae12cb8
Clark Ward <67551381+ClarkWard711@users.noreply.github.com>
2025-01-02 00:03:51 +0800

    0.4.0.17
    完善骑士基础技能

--- Assets/Scripts/SkillsSystem/SkillHolder/DreamlandKnightHolder.cs
+++ Assets/Scripts/SkillsSystem/SkillHolder/DreamlandKnightHolder.cs
@@ -5,40 +5,69 @@ using UnityEngine;
 
 public class DreamlandKnightHolder : JobSkillHolder
 {
-    public static DreamlandKnightHolder Instance;
-    public int DreamCount;
-    public int DreamDFallenCount = 1;
-    public override void Awake()
-    {
-        base.Awake();
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-    }
-    #region  基础
-    public IEnumerator dreamFallen(int SpCost)
-    {
-        BattleSetting.Instance.canChangeAction = false;
-        BattleSetting.Instance.isChooseFinished = false;
-        BattleSetting.Instance.State = BattleState.Middle;
-        BattleSetting.Instance.CurrentActUnit.GetComponent<GivingData>().currentSP -= SpCost;
-        DreamCount += DreamDFallenCount;
-        DreamDFallenCount++;
-        StartCoroutine(BattleSetting.Instance.ShowActionText("坠梦"));
-        yield return new WaitForSeconds(1f);
-        BattleSetting.Instance.ActionEnd();
-    }
+	public static DreamlandKnightHolder Instance;
+	public int DreamCount;
+	public int DreamFallenCount = 1;
+	public override void Awake()
+	{
+		base.Awake();
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+	}
+	#region  基础
+	public IEnumerator dreamFallen(int SpCost)
+	{
+		BattleSetting.Instance.canChangeAction = false;
+		BattleSetting.Instance.isChooseFinished = false;
+		BattleSetting.Instance.State = BattleState.Middle;
+		BattleSetting.Instance.CurrentActUnit.GetComponent<GivingData>().currentSP -= SpCost;
+		DreamCount += DreamFallenCount;
+		DreamFallenCount++;
+		StartCoroutine(BattleSetting.Instance.ShowActionText("坠梦"));
+		yield return new WaitForSeconds(1f);
+		BattleSetting.Instance.ActionEnd();
+	}
 
-    public IEnumerator oneiromancy(int SpCost)
-    {
-        BattleSetting.Instance.canChangeAction = false;
-        BattleSetting.Instance.isChooseFinished = false;
-        BattleSetting.Instance.State = BattleState.Middle;
-        BattleSetting.Instance.CurrentActUnit.GetComponent<GivingData>().currentSP -= SpCost;
-        StartCoroutine(BattleSetting.Instance.ShowActionText("解梦"));
-        yield return new WaitForSeconds(1f);
-        BattleSetting.Instance.ActionEnd();
-    }
-    #endregion
+	public IEnumerator oneiromancy(int SpCost)
+	{
+		BattleSetting.Instance.canChangeAction = false;
+		BattleSetting.Instance.isChooseFinished = false;
+		BattleSetting.Instance.State = BattleState.Middle;
+		BattleSetting.Instance.CurrentActUnit.GetComponent<GivingData>().currentSP -= SpCost;
+		foreach (var player in BattleSetting.Instance.RemainingPlayerUnits)
+		{
+			var tag = PhysicalAtkUp.CreateInstance<PhysicalAtkUp>();
+			tag.TurnAdd = DreamCount;
+			tag.TurnLast = DreamCount;
+			player.GetComponent<GivingData>().AddTagToCharacter(tag);
+		}
+		DreamCount = 0;
+		StartCoroutine(BattleSetting.Instance.ShowActionText("解梦"));
+		yield return new WaitForSeconds(1f);
+		BattleSetting.Instance.ActionEnd();
+	}
+	#endregion
+
+	#region Advanced
+	public IEnumerator dreamProtect(int SpCost)
+	{
+		BattleSetting.Instance.canChangeAction = false;
+		BattleSetting.Instance.isChooseFinished = false;
+		BattleSetting.Instance.State = BattleState.Middle;
+		BattleSetting.Instance.CurrentActUnit.GetComponent<GivingData>().currentSP -= SpCost;
+		foreach (var player in BattleSetting.Instance.RemainingPlayerUnits)
+		{
+			var tag = PhysicalAtkUp.CreateInstance<PhysicalAtkUp>();
+			tag.TurnAdd = DreamCount;
+			tag.TurnLast = DreamCount;
+			player.GetComponent<GivingData>().AddTagToCharacter(tag);
+		}
+		DreamCount = 0;
+		StartCoroutine(BattleSetting.Instance.ShowActionText("解梦"));
+		yield return new WaitForSeconds(1f);
+		BattleSetting.Instance.ActionEnd();
+	}
+	#endregion
 }
